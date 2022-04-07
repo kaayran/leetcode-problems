e799ad5f79dafbb8623d7f99854c3842771a0be2
kaayran <dodororo132@gmail.com>
2022-04-07 05:38:53 +0300

    solve problem n69

---
+++ sqrt-x-69/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program {
+        public static void Main() {
+            int x = 2147483612;
+
+            var result = MySqrt(x);
+
+            Console.WriteLine(result);
+        }
+
+        public static int MySqrt(int x) {
+            for (int i = 0; i < 46340; i++)
+            {
+                int lessMul = i * i;
+                int moreMul = (i + 1) * (i + 1);
+
+                if (lessMul == x) return i;
+                if (moreMul == x) return i + 1;
+
+                if (lessMul < x && moreMul > x) return i;
+            }
+
+            return 46340;
+        }
+    }
+}
