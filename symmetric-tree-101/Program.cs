7080da7296f3658bcc2f0fa437ebd4ff5db65b80
kaayran <dodororo132@gmail.com>
2022-04-11 11:28:58 +0300

    sovle problem n101 with symmetric trees

--- symmetric-tree-101/Program.cs
+++ symmetric-tree-101/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Solution {
 	public class TreeNode {
@@ -15,13 +15,65 @@ namespace Solution {
 	public class Solution {
 		public static void Main() {
 
+			var node4 = new TreeNode(4, null, null);
+			var node3 = new TreeNode(4, null, null);
+			var node2 = new TreeNode(2, null, node4);
+			var node1 = new TreeNode(2, node3, null);
 
-			var result = IsSymmetric(root);
+			var root = new TreeNode(1, node1, node2);
+
+			var result = IsSymmetric(null);
 
 			Console.WriteLine(result);
 		}
-	    public bool IsSymmetric(TreeNode root) {
-	        
-	    }
+
+	    public static bool IsSymmetric(TreeNode root) {
+			if (root == null) return true;
+
+			if (root.left == null && root.right == null) return true;
+			if (root.left == null && root.right != null) return false;
+			if (root.left != null && root.right == null) return false;			
+
+			var currP = root.left;
+			var currQ = root.right;
+
+	        var nodesP = new Stack<TreeNode>();
+            var nodesQ = new Stack<TreeNode>();
+
+            while (currP != null && currQ != null || nodesP.Count != 0 || nodesQ.Count != 0)
+            {               
+                while (currP != null && currQ != null)
+                {
+                    if (currP.val != currQ.val) return false;
+
+                    nodesP.Push(currP);
+                    nodesQ.Push(currQ);
+
+                    currP = currP.left;
+                    currQ = currQ.right;
+                }
+                
+                if (CheckNodes(currP, currQ)) {
+                    return false;
+                } 
+
+                currP = nodesP.Pop();
+                currQ = nodesQ.Pop();
+
+                currP = currP.right;
+                currQ = currQ.left;
+
+                if (currP != null && currQ != null)
+                {
+                    if (currP.val != currQ.val) return false;
+                }
+            }
+
+            return !CheckNodes(currP, currQ);
+        }
+
+        public static bool CheckNodes(TreeNode p, TreeNode q) {
+            return p != null || q != null;
+        }
 	}
 }
