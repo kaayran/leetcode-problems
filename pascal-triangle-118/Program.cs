5fdc16fd7f381807fab1be1fdd6710d8f1c35894
kaayran <dodororo132@gmail.com>
2022-04-14 13:38:30 +0300

    write some code to solve problem n118 with pascals triangle

---
+++ pascal-triangle-118/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program {
+        public static void Main(){
+            var numRows = 5;
+
+            var result = Generate(numRows);
+
+            foreach (var list in result)
+            {
+                foreach (var n in list)
+                {
+                    Console.Write(n);
+                    Console.Write("\t");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+        public static List<List<int>> Generate(int numRows) {
+            var triangle = new List<List<int>>();
+            
+            for (int i = 0; i < numRows; i++)
+            {
+                triangle.Add(new List<int>());
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    triangle[i].Add(1);
+                }
+            }
+
+            return triangle;
+        }
+
+    }
+}
