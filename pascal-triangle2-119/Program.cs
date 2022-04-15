3d177a26f186fc0f71566e0fd2979127f35cb428
kaayran <dodororo132@gmail.com>
2022-04-15 15:16:30 +0300

    solve problem n119

---
+++ pascal-triangle2-119/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program {
+        public static void Main(){
+            var rowIndex = 0;
+
+            var result = GetRow(rowIndex);
+
+            foreach (var n in result)
+            {
+                Console.Write(n);
+                Console.Write("\t");
+            }
+        }
+
+        public static IList<int> GetRow(int rowIndex) {
+            IList<int> temp = new List<int>();
+            IList<int> row = new List<int>();
+                       
+            for (int i = 0; i < rowIndex + 1; i++)
+            {
+                row.Clear();
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    if (j == 0 || j == i) {
+                        row.Add(1);    
+                    }
+                    else
+                    {
+                        var value = temp[j - 1] + temp[j];
+                        row.Add(value);
+                    }                                    
+                }
+
+                temp = new List<int>(row);
+            }
+
+            return row;
+        }
+
+    }
+}
