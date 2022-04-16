8bb0f9ccbbdd0a6abce0b5c83464bb389a981901
kaayran <dodororo132@gmail.com>
2022-04-16 15:26:27 +0300

    solve problem n121

---
+++ best-time-to-buy-stock-121/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program {
+        public static void Main() {
+            var prices = new int[] {7, 6, 4, 11, 1};
+
+            var profit = MaxProfit(prices);
+
+            Console.WriteLine(profit);
+        }
+
+        public static int MaxProfit(int[] prices) {
+            var min_price = int.MaxValue;
+            var profit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < min_price) {
+                    min_price = prices[i];
+                } 
+                else if (prices[i] - min_price > profit) {
+                    profit = prices[i] - min_price;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
