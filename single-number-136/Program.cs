9e983953971b3a7fbfc11e5c9f006b0f24bc7d1e
kaayran <dodororo132@gmail.com>
2022-04-17 17:44:32 +0300

    solve problem with single number

---
+++ single-number-136/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var nums = new int[] { 4, 1, 2, 1, 2 };
+
+            var result = SingleNumber(nums);
+
+            Console.Write(result);
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            int singleNumber = 0;
+
+            foreach (int num in nums)
+            {
+                singleNumber ^= num;
+            }
+
+            return singleNumber;
+        }
+    }
+}
