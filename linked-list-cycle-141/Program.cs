a800523263d94251982bcfda3babafa3b46e7343
kaayran <dodororo132@gmail.com>
2022-04-18 22:28:49 +0300

    solve problem n141

---
+++ linked-list-cycle-141/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+namespace Solution
+{
+    public class Program
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var next = head;
+            var links = new HashSet<ListNode>();
+
+            while (next != null)
+            {
+                if (!links.Contains(next.next))
+                {
+                    links.Add(next.next);
+                    next = next.next;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
