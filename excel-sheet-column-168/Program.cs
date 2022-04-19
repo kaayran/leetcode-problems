58e2eabfcdfd95cf7dc4ff9911ebcd66d67cfa9e
kaayran <dodororo132@gmail.com>
2022-04-19 16:44:58 +0300

    solve problem with excel columns

---
+++ excel-sheet-column-168/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Solution
+{
+    public class Program {
+        public static void Main() {
+            var columnNumber = 701;
+
+            var result = ConvertToTitle(columnNumber);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ConvertToTitle(int columnNumber) {
+            var strBuilder = new StringBuilder();
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                var letter = Convert.ToChar(columnNumber % 26 + 'A');
+                strBuilder.Append(letter);
+
+                columnNumber /= 26;
+            }
+
+            var res = new string(strBuilder.ToString().ToCharArray().Reverse().ToArray());
+
+            return res;
+        }
+    }
+}
