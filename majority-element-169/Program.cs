1529efc916480360b43bb09afeb77c7e04c3e8fc
kaayran <dodororo132@gmail.com>
2022-04-20 04:44:33 +0300

    fast-forward commit, solved with dic<int, int>

---
+++ majority-element-169/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var nums = new int[] { 2 };
+
+            var result = MajorityElement(nums);
+
+            Console.WriteLine(result);
+        }
+
+        private static int MajorityElement(int[] nums)
+        {
+            var amounts = new Dictionary<int, int>();
+            var res = nums[0];
+
+            foreach (var num in nums)
+            {
+                if (amounts.ContainsKey(num))
+                {
+                    amounts[num] += 1;
+
+                    if (amounts[num] > nums.Length / 2)
+                    {
+                        res = num;
+                        return res;
+                    }
+                }
+                else
+                {
+                    amounts.Add(num, 1);
+                }
+            }
+
+            return res;
+        }
+    }
+}
