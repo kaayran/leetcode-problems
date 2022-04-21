b42bb9e9b805707a62f6cec339169d156748660a
kaayran <dodororo132@gmail.com>
2022-04-21 19:11:26 +0300

    solve fckk problem with bits

---
+++ reverse-bits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            UInt32 n = 43261596;
+
+            var result = ReverseBits(n);
+
+            Console.WriteLine(result);
+        }
+
+        public static uint ReverseBits(uint n)
+        {
+            var converted = Convert.ToString(n, 2).PadLeft(32, '0');
+
+            var chars = converted.ToCharArray();
+
+            Array.Reverse(chars);
+
+            return Convert.ToUInt32(new string(chars), 2);
+        }
+    }
+}
