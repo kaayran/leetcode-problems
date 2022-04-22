9478203e4e66d21bda4def5e2347a7d7111a1af3
kaayran <dodororo132@gmail.com>
2022-04-22 05:12:46 +0300

    solve problem with iso-strings with Dictionary mapping

---
+++ isomorphic-string-205/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var s = "a";
+            var t = "a";
+
+            Console.WriteLine(IsIsomorphic(s, t));
+        }
+        public static bool IsIsomorphic(string s, string t)
+        {
+            var chars = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!chars.ContainsKey(s[i]))
+                {
+                    if (chars.ContainsValue(t[i])) return false;
+                    
+                    chars.Add(s[i], t[i]);
+                }
+                else
+                {
+                    if (chars[s[i]] != t[i]) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
