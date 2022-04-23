a962fff794127a45a380451e09756c4d0a9bda8e
kaayran <dodororo132@gmail.com>
2022-04-23 08:38:22 +0300

    solve problem with duplicates by HashSet & Distinct()

---
+++ contains-duplicate-217/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var nums = new int[] { 1, 1, 1 };
+
+            Console.WriteLine(ContainsDuplicateByHashSet(nums));
+        }
+
+        private static bool ContainsDuplicate(int[] nums)
+        {
+            var distLength = nums.Distinct().Count();
+            var numsLength = nums.Count();
+
+            return distLength < numsLength ? true : false;
+        }
+
+        private static bool ContainsDuplicateByHashSet(int[] nums)
+        {
+            var hash = new HashSet<int>(nums);
+
+            return hash.Count() < nums.Count() ? true : false;
+        }
+    }
+}
