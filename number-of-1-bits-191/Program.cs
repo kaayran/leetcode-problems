0ed08fd340ff8de9081893fe137d6c602e5f4a74
kaayran <dodororo132@gmail.com>
2022-04-24 04:41:20 +0300

    solve problem with 1 bits in number by two algos

---
+++ number-of-1-bits-191/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Solution
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            UInt32 n = 123;
+
+            Console.WriteLine("Count: {0}", HammingWeightLogical(n));
+        }
+
+        private static int HammingWeight(uint n)
+        {
+            var conv = Convert.ToString(n, 2);
+            Console.WriteLine("In binary: {0}", conv);
+
+            var count = conv.Count(ch => ch == '1');
+
+            return count;
+        }
+        private static int HammingWeightLogical(uint n)
+        {
+            uint count = 0;
+
+            while (n is not 0)
+            {
+                count += n % 2;
+                n >>= 1;
+            }
+
+            return (int)count;
+        }
+    }
+}
