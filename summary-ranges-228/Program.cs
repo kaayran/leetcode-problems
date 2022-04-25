3af536d3c57dc92eed6093efcf6a9be48a5648c8
kaayran <dodororo132@gmail.com>
2022-04-25 11:42:53 +0300

    solve boring problem with array w/ uniq numbers;

---
+++ summary-ranges-228/Program.cs
@@ -0,0 +1,26 @@
+﻿var nums = new[] {};
+
+List<string> ranges = new IList<string>();
+
+if (nums.Length == 0) return ranges;
+
+int i = 0, n = nums.Length;
+
+while (i < n)
+{
+    int start, end;
+
+    start = nums[i];
+    while (i + 1 < n && nums[i + 1] == nums[i] + 1) i++;
+
+    end = nums[i];
+
+    if (start == end)
+        ranges.Add(start + "");
+    else
+        ranges.Add(start + "->" + end);
+
+    i++;
+}
+
+ranges.ForEach(s => Console.WriteLine(s));
