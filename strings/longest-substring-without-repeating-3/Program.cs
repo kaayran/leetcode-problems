fc9a18ed0b5f13dbe0b91b8a6510d0a84069e06a
kaayran <dodororo132@gmail.com>
2022-05-04 11:36:10 +0300

    create dir for string problems, solve one of it

---
+++ strings/longest-substring-without-repeating-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Solution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "dvdf";
+            System.Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+
+        private static int LengthOfLongestSubstring(string s)
+        {
+            var i = 0;
+            var j = 0;
+            var length = 0;
+            var chars = new HashSet<char>();
+
+            while (j < s.Length)
+            {
+                if (!chars.Contains(s[j]))
+                {
+                    chars.Add(s[j]);
+                    j++;
+                    length = Math.Max(chars.Count, length);
+                }
+                else
+                {
+                    chars.Remove(s[i]);
+                    i++;
+                }
+            }
+
+            return length;
+        }
+    }
+}
